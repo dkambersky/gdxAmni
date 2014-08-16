b2bd0b1381f06d961388367d93dc0ced8451eea0
David Kamberský <david.kambersky@gmail.com>
2014-08-16 07:16:39 +0200

    Tile state (basic) functionality complete :congratulations:

--- Amniora/src/me/xcabbage/amniora/nonstatic/State.java
+++ Amniora/src/me/xcabbage/amniora/nonstatic/State.java
@@ -10,8 +10,21 @@ package me.xcabbage.amniora.nonstatic;
 
 /**
  * @author David
- *
+ * 
  */
 public enum State {
 
+	// initial state, vacant
+	EMPTY,
+
+	// soil with potential for upgraded structures
+	EMPTY_FAST_QUARRY, EMPTY_FAST_RAX,
+
+	// occupied with buildings
+
+	// uniques
+	OCCUPIED_UNIQUE_ATTACK, OCCUPIED_UNIQUE_DEFENSE, OCCUPIED_UNIQUE_QUARRY, OCCUPIED_UNIQUE_SIGHT,
+
+	// buildings
+	OCCUPIED_FAST_QUARRY, OCCUPIED_FAST_RAX, OCCUPIED_RAX, OCCUPIED_QUARRY, OCCUPIED_GEOLAB, OCCUPIED_BANNER;
 }
--- Amniora/src/me/xcabbage/amniora/nonstatic/Tile.java
+++ Amniora/src/me/xcabbage/amniora/nonstatic/Tile.java
@@ -1,4 +1,5 @@
-/** The Tile.java class responsible for 
+/** The Tile.java class from which individual tile objects are generated. 
+ *  A Tile object holds the info for a single tile, parallel to the rendered faces of the icosahedron. 
  *
  * @author xCabbage [github.com/xcabbage]
  *
@@ -13,20 +14,24 @@ package me.xcabbage.amniora.nonstatic;
  * 
  */
 public class Tile {
-	
-	//The tile's allegiance to a side; 0 = neutral, 1 = blue, 2 = orange
+
+	// The tile's allegiance to a side; 0 = neutral, 1 = blue, 2 = orange
 	int allegiance;
-	
-	//The tile's type - generated once in the game's beginning. Can be normal (0) or specials such as rich soil or combat shrines.
+
+	// The tile's type - generated once in the game's beginning. Can be normal
+	// (0) or specials such as rich soil or combat shrines.
 	Type type;
-	
-	//The immediate state of the tile. Whether any building is currently standing on it, etc
+
+	// The immediate state of the tile. Whether any building is currently
+	// standing on it, etc
 	State state;
-	
-	//The friendly forces present on the tile.
+
+	// The friendly forces present on the tile.
 	int armyFootman, armySamurai, armyReaver;
 
-	
-	
-
+	public Tile() {
+		allegiance = 0;
+		type = Type.Normal;
+		state = State.EMPTY;
+	}
 }
